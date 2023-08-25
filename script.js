d97cb7a4046f5fcd1d21e7136d62205c8f2e9689
Rithvik Bonagiri <rithvik.bonagiri@gmail.com>
2023-08-25 14:57:12 -0400

    Change Hover to Drag Mouse to Paint

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const pxCont = document.getElementById('pixelContainer');
+let isMouseDown = false;
 
 function createGrid(num) {
     pxCont.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     pxCont.style.gridTemplateRows = `repeat(${num}, 1fr)`;
 
-    for(let i = 0; i < num*num; i++) {
+    for (let i = 0; i < num * num; i++) {
         const pxl = document.createElement("div");
         pxl.classList.add('pxl');
         //pxl.textContent = `${i}`;
-        pxl.onmouseover = function() {
-            pxl.classList.add('hovered');
+        pxl.onmousedown = function () { isMouseDown = true };
+        pxl.onmouseup = function () { isMouseDown = false };
+
+
+
+        pxl.onmouseover = function () {
+            if (isMouseDown) {
+                pxl.classList.add('hovered');
+            }
         }
         pxCont.appendChild(pxl);
     }
 }
 
 createGrid(16);
+
+
 
