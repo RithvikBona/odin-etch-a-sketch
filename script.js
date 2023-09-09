254a99c6a1a8ef39c5d8092a3842201fafe21965
Rithvik Bonagiri <rithvik.bonagiri@gmail.com>
2023-09-09 16:18:45 -0400

    Add Rainbow Random Color Mode (In Progress)

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const colorPicker = document.getElementById('colorPicker');
 let penColor = colorPicker.value;
 let isMouseDown = false;
 let isEraser =  false;
+let isRainbow = false;
 
 function createGrid(num) {
     pxCont.replaceChildren();
@@ -24,6 +25,9 @@ function createGrid(num) {
             if (isMouseDown) {
                 if(isEraser) {
                     pxl.style.backgroundColor = 'white';
+                } else if(isRainbow){
+                    let RGBVals = getRandomColor();
+                    pxl.style.backgroundColor = `#${RGBVals.r}${RGBVals.g}${RGBVals.b}`;
                 } else {
                     pxl.style.backgroundColor = penColor;
                 }
@@ -33,6 +37,19 @@ function createGrid(num) {
         pxCont.appendChild(pxl);
     }
 }
+
+//Want to create pleasing colors, so using HSV and the Golden Ratio Constant for even color
+//spacing
+let h = Math.random(); //start with initial randomness
+function getRandomColor() {
+    goldenRatioConst = 0.618033988749895;
+    h += goldenRatioConst; //continously add golden Ratio Constant which makes even spacing
+    h %= 1; //so decimal between 0 and 1
+    //let h = Math.random();
+    return HSVtoRGB(h, 0.96, 0.92);
+}
+
+
 
 createGrid(16);
 
@@ -47,7 +64,9 @@ slider.addEventListener('input', (e) => {
 const eraserBtn = document.querySelector('.eraserBtn');
 eraserBtn.addEventListener('click', (e) => {
     isEraser = !isEraser;
-    eraserBtn.classList.toggle("btnPressed")
+    eraserBtn.classList.toggle("btnPressed");
+    isRainbow = false; //no dupes
+    rainbowBtn.classList.remove("btnPressed");
 });
 
 const clearBtn = document.querySelector(".clearBtn");
@@ -58,3 +77,43 @@ clearBtn.addEventListener('click', (e) => {
 colorPicker.addEventListener('input', (e) => {
     penColor = colorPicker.value;
 })
+
+const rainbowBtn = document.querySelector(".rainbowBtn");
+rainbowBtn.addEventListener('click', (e) => {
+    isRainbow = !isRainbow;
+    rainbowBtn.classList.toggle("btnPressed")
+    //
+    isEraser = false;
+    eraserBtn.classList.remove("btnPressed");
+});
+
+
+
+
+
+
+//Credit to Stack Overflow User Parthik Gosar
+function HSVtoRGB(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (arguments.length === 1) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.round(r * 255),
+        g: Math.round(g * 255),
+        b: Math.round(b * 255)
+    };
+}
