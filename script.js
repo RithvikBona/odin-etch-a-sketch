5ff91342858c7809d6b61abb3fbf12471bea9c9e
Rithvik Bonagiri <rithvik.bonagiri@gmail.com>
2023-09-10 15:58:57 -0400

    Add Darken Mode

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const sliderText = document.querySelector('.sliderText');
 const pxCont = document.getElementById('pixelContainer');
 const colorPicker = document.getElementById('colorPicker');
 let penColor = colorPicker.value;
+//button toggles
 let isMouseDown = false;
-let isEraser =  false;
+let isEraser = false;
 let isRainbow = false;
+let isDarken = false;
 
 function createGrid(num) {
     pxCont.replaceChildren();
@@ -15,7 +17,7 @@ function createGrid(num) {
     for (let i = 0; i < num * num; i++) {
         const pxl = document.createElement("div");
         pxl.classList.add('pxl');
-        pxl.setAttribute('id', `pxl${i}`);
+        //pxl.setAttribute('id', `pxl${i}`);
         pxl.onmousedown = function () { isMouseDown = true };
         pxl.onmouseup = function () { isMouseDown = false };
 
@@ -23,20 +25,41 @@ function createGrid(num) {
 
         pxl.onmouseover = function () {
             if (isMouseDown) {
-                if(isEraser) {
+                if (isEraser) {
                     pxl.style.backgroundColor = 'white';
-                } else if(isRainbow){
+                } else if (isRainbow) {
                     let RGBVals = getRandomColor();
                     pxl.style.backgroundColor = `rgb(${RGBVals.join(',')})`
+                } else if (isDarken) {
+                    
+                    let rgbNums = pxl.style.backgroundColor.substring(4, pxl.style.backgroundColor.length - 1);
+                    pxl.style.backgroundColor = darken(rgbNums);
                 } else {
                     pxl.style.backgroundColor = penColor;
                 }
-                
+
             }
         }
         pxCont.appendChild(pxl);
     }
 }
+
+function darken(rgbNums) {
+    let rgbArr = (rgbNums != "") ? (rgbNums.split(',')) : ["255", "255", "255"];
+
+    let rInit = rgbArr[0].trim();
+    let gInit = rgbArr[1].trim();
+    let bInit = rgbArr[2].trim();
+
+    //reduce rgb vals by 17 each time, or set to 0 if negative
+    let r = (rInit > 17) ? (rInit - 17) : 0;
+    let g = (gInit > 17) ? (gInit - 17) : 0;
+    let b = (bInit > 17) ? (bInit - 17) : 0;
+
+    return `rgb(${r},${g},${b})`;
+}
+
+
 
 //Want to create pleasing colors, so using HSV and the Golden Ratio Constant for even color
 //spacing
@@ -64,8 +87,11 @@ const eraserBtn = document.querySelector('.eraserBtn');
 eraserBtn.addEventListener('click', (e) => {
     isEraser = !isEraser;
     eraserBtn.classList.toggle("btnPressed");
-    isRainbow = false; //no dupes
+    //
+    isRainbow = false;
     rainbowBtn.classList.remove("btnPressed");
+    isDarken = false;
+    darkenBtn.classList.remove("btnPressed");
 });
 
 const clearBtn = document.querySelector(".clearBtn");
@@ -84,6 +110,19 @@ rainbowBtn.addEventListener('click', (e) => {
     //
     isEraser = false;
     eraserBtn.classList.remove("btnPressed");
+    isDarken = false;
+    darkenBtn.classList.remove("btnPressed");
+});
+
+const darkenBtn = document.querySelector(".darkenBtn");
+darkenBtn.addEventListener('click', (e) => {
+    isDarken = !isDarken;
+    darkenBtn.classList.toggle("btnPressed")
+    //
+    isEraser = false;
+    eraserBtn.classList.remove("btnPressed");
+    isRainbow = false;
+    rainbowBtn.classList.remove("btnPressed");
 });
 
 
@@ -95,24 +134,24 @@ rainbowBtn.addEventListener('click', (e) => {
 //https://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
 function hslToRgb(h, s, l) {
     let r, g, b;
-  
+
     if (s === 0) {
-      r = g = b = l; // achromatic
+        r = g = b = l; // achromatic
     } else {
-      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-      const p = 2 * l - q;
-      r = hueToRgb(p, q, h + 1.0/3.0);
-      g = hueToRgb(p, q, h);
-      b = hueToRgb(p, q, h - 1.0/3.0);
+        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        const p = 2 * l - q;
+        r = hueToRgb(p, q, h + 1.0 / 3.0);
+        g = hueToRgb(p, q, h);
+        b = hueToRgb(p, q, h - 1.0 / 3.0);
     }
-  
+
     return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-  }  
-  function hueToRgb(p, q, t) {
+}
+function hueToRgb(p, q, t) {
     if (t < 0) t += 1;
     if (t > 1) t -= 1;
-    if (t < 1.0/6.0) return p + (q - p) * 6 * t;
-    if (t < 1.0/2.0) return q;
-    if (t < 2.0/3.0) return p + (q - p) * (2.0/3.0 - t) * 6;
+    if (t < 1.0 / 6.0) return p + (q - p) * 6 * t;
+    if (t < 1.0 / 2.0) return q;
+    if (t < 2.0 / 3.0) return p + (q - p) * (2.0 / 3.0 - t) * 6;
     return p;
-  }
+}
